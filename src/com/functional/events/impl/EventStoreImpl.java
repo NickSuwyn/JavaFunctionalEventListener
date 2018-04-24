8a8867caa100564379259dc16b49bcdc347ca0f0
Suwyn <suwynni@repsrv.com>
2018-04-24 15:06:26 -0700

    made EventStoreImpl more readable

--- src/com/functional/events/impl/EventStoreImpl.java
+++ src/com/functional/events/impl/EventStoreImpl.java
@@ -2,6 +2,9 @@ package com.functional.events.impl;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 import com.functional.events.Event;
 import com.functional.events.EventStore;
@@ -16,9 +19,19 @@ public class EventStoreImpl implements EventStore {
 	
 	public void executeEvent(String name) {
 		events.keySet().stream()
-			.filter(x -> x.equalsIgnoreCase(name))
+			.filter(matchesString(name))
 			.findFirst()
-			.ifPresent(x -> events.get(x).execute());
+			.ifPresent(executeEvent());
+	}
+	
+	//private Function<String, Predicate<String>> matchesString = x -> y -> x.equalsIgnoreCase(y);
+	
+	private Predicate<String> matchesString(String string) {
+		return x -> x.equalsIgnoreCase(string);
+	}
+	
+	private Consumer<String> executeEvent() {
+		return x -> events.get(x).execute();
 	}
 
 }
